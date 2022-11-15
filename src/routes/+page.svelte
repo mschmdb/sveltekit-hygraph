ac3b6dac4cfc6aad39cffcb5ba18a3c853581434
mschmdb <113426956+mschmdb@users.noreply.github.com>
2022-11-15 07:16:37 +0100

    New Branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,11 @@
 	import Hero from '../components/Hero.svelte'
 	import Header from '../components/HeaderIndex.svelte'
 	import { fly } from 'svelte/transition'
+	import InfiniteScroll from 'svelte-infinite-scroll'
 
 	/** @type {import('./$types').PageData} */
 	export let data
-	let first = 3
+	let first = 4
 </script>
 
 <svelte:head>
@@ -66,37 +67,37 @@
 						</div>
 					</div>
 				{/each}
-			</div>
-			{#if first < data.posts.length}
-				<div class="flex flex-col justify-center items-center py-8">
-					<button
-						on:click={() => (first = first + 3)}
-						class="flex bg-[#890c58] self-center justify-center w-full px-2 py-1 text-white transition-colors duration-300 transform rounded-md focus:outline-none sm:w-auto sm:mx-1 focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="w-5 h-5 mx-1"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
+				{#if first < data.posts.length}
+					<div class="flex flex-col justify-center items-center py-8">
+						<button
+							on:click={() => (first = first + 3)}
+							class="flex bg-[#890c58] self-center justify-center w-full px-2 py-1 text-white transition-colors duration-300 transform rounded-md focus:outline-none sm:w-auto sm:mx-1 focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-40"
 						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-							/>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-							/>
-						</svg>
-						<span class="mx-4"> load more </span>
-					</button>
-				</div>
-			{/if}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="w-5 h-5 mx-1"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+								/>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+								/>
+							</svg>
+							<span class="mx-4"> load more </span>
+						</button>
+					</div>
+				{/if}
+			</div>
 		</section>
 	</main>
 </div>
